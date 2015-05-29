454d6a5ab407a8b34db81c207848ae7c29c3856b
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-05-29 20:43:00 +0800

    project.clj: update description
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clj-jssc "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple serial port comms library. Wraps jssc."
+  :url "http://github.com/coldnew/clj-jssc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.scream3r/jssc "2.8.0"]])
