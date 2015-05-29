49f6e9c6e3bcdb104ce428b9b07e8bdccb13e7d4
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-05-29 20:44:25 +0800

    Add prototype
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- src/clj_jssc/core.clj
+++ src/clj_jssc/core.clj
@@ -1,6 +1,38 @@
-(ns clj-jssc.core)
+(ns clj-jssc.core
+  (:import [jssc
+            SerialPortList
+            SerialPort
+            SerialPortException]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn port-ids
+  "Returns a seq representing all port identifiers visible to the system"
+  []
+  (seq
+   (SerialPortList/getPortNames)))
+
+(defn list-ports
+  "Print out the available ports with an index number for future reference
+  with (port-at <i>)."
+  []
+  (apply println (port-ids)))
+
+
+(defn close
+  "Close an open port."
+  [port]
+  (.closePort port))
+
+(defn open
+  ([path] (open path 115200))
+  ([path baud-rate]
+   (try
+     (let [serial (SerialPort. path)]
+       (.openPort serial)
+       (.setParams serial
+                   SerialPort/BAUDRATE_9600
+                   SerialPort/DATABITS_8
+                   SerialPort/STOPBITS_1
+                   SerialPort/PARITY_NONE)
+       serial)
+     (catch SerialPortException e
+       (throw (Exception. (str "Sorry, couldn't connect to the port with path " path )))))))
